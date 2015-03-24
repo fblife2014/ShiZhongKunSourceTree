aedcfd0ac1a121e522a66de4f4b44a4a9bf1f1e2
fblife2014 <shizhongkun@fblife.com>
2015-03-24 16:00:00 +0800

    FirstCommit

---
+++ ShowCar/ShowCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowCar
+//
+//  Created by szk on 15/3/24.
+//  Copyright (c) 2015年 shizhongkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShowCar/ShowCar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShowCar
+//
+//  Created by szk on 15/3/24.
+//  Copyright (c) 2015年 shizhongkun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
